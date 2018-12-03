4dcd1989905140ea6108b31dccb84cd390d59497
zerlee <1183391310@qq.com>
2018-12-03 19:57:25 +0800

    smtp发送邮件

---
+++ sendmail_demo.py
@@ -0,0 +1,29 @@
+#!/bin/bash/python
+from email.mime.text import MIMEText #构造邮件模块
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+import smtplib #发送邮件模块
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
+from_addr = '18861351153@163.com'
+password = '520224'
+to_addr = '1183391310@qq.com,18861351153@163.com'
+smtp_server = 'smtp.163.com'
+
+msg = MIMEText('hello,send by Python...', 'plain', 'utf-8') #邮件正文,纯文本,utf-8编码
+msg['From'] = _format_addr('python爱好者 <%s>' % from_addr)
+msg['To'] = to_addr
+msg['Subject'] = Header('来自SMTP的问候...', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, to_addr.split(','), msg.as_string())
+server.quit()
+
